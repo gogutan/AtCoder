01c2b518791236d6607b765348aec2535c04bffc
gogutan <50437473+gogutan@users.noreply.github.com>
2024-07-17 19:55:47 +0900

    Fix abc184

--- abc184Csuper.rb
+++ abc184Csuper.rb
@@ -2,25 +2,27 @@ def reachable? (r1, c1, r2, c2)
   r1 + c1 == r2 + c2 || r1 - c1 == r2 - c2 || ((r1 - r2).abs + (c1 - c2).abs) <= 3
 end
 
-r1, c1 = gets.split.map(&:to_i)
-r2, c2 = gets.split.map(&:to_i)
-step = 0
+def distance(r1, c1, r2, c2)
+  (r1 - r2).abs + (c1 - c2).abs
+end
 
-until reachable?(r1, c1, r2, c2)
-  step += 1
-  r_dist = (r1 - r2).abs
-  c_dist = (c1 - c2).abs
-  movable = (r_dist + c_dist) / 2
-  if r1 < r2
-    r1 += movable
+def diff(r1, c1, r2, c2)
+  ((r1 - r2).abs - (c1 - c2).abs).abs
+end
+
+def step(r1, c1, r2, c2)
+  if r1 == r2 && c1 == c2
+    0
+  elsif reachable?(r1, c1, r2, c2)
+    1
+  elsif distance(r1, c1, r2, c2).even? || distance(r1, c1, r2, c2) <= 6 || diff(r1, c1, r2, c2) <= 4
+    2
   else
-    r2 += movable
-  end
-  if c1 < c2
-    c1 += movable
-  else
-    c2 += movable
+    3
   end
 end
 
-puts (r1 == r2 && c1 == c2) ? step : step + 1
+r1, c1 = gets.split.map(&:to_i)
+r2, c2 = gets.split.map(&:to_i)
+
+puts step(r1, c1, r2, c2)
